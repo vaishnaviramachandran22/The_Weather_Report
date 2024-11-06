72def6322b5bed0898e81984ddd9002a75fd24c5
Vaishnavi Ramachandran <vaishnaviramachandran22@gmail.com>
2024-11-06 14:00:06 -0500

    api key changed . enter your own key

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const apiKey = "f672665040b8e271831993c65ed976e3";
+const apiKey = 122334454;//enter your Api KEY 
 const apiUrl ="https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 
 const searchBox = document.querySelector(".search input");
